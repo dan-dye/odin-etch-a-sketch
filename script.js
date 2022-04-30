9eadd5b5ca3371516780f1a33c048eed3f910aae
dan-dye <danrdye@gmail.com>
2022-04-30 18:14:54 -0400

    Add pixel matrix

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 const container = document.querySelector('.container');
+const button = document.querySelector('button');
+
+button.addEventListener('click', function (e) {
+  let width = prompt("Enter new pixel width: ");
+  createPixelMatrix(width);
+})
+
+createPixelMatrix(16);
+
+function createPixelMatrix(pixels, color) {
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+    console.log('removed')
+  }
+  for (let i = 0; i < pixels * pixels; i++) {
+    let newElement = document.createElement('div');
+    newElement.textContent = i;
+    newElement.style.backgroundColor = color;
+    container.appendChild(newElement);
+  }
+}
