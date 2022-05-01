ee62392e622186420f086dcb0b945e7d93802596
dan-dye <danrdye@gmail.com>
2022-05-01 12:22:38 -0400

    Fix aspect ration of pixels and change hover effect to leave a trail

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const container = document.querySelector('.container');
 const button = document.querySelector('button');
 
+
 button.addEventListener('click', function (e) {
   let width = prompt("Enter new pixel width: ");
   createPixelMatrix(width);
@@ -13,11 +14,14 @@ function createPixelMatrix(pixels) {
     container.removeChild(container.firstChild);
   }
 
-  // container.style.gridTemplateColumns = `repeat(${pixels}, 1fr)`;
-  // container.style.gridTemplateRows = `repeat(${pixels}, 1fr)`;
+  container.style.gridTemplateColumns = `repeat(${pixels}, 1fr)`;
+  container.style.gridTemplateRows = `repeat(${pixels}, 1fr)`;
 
   for (let i = 0; i < pixels * pixels; i++) {
     let newElement = document.createElement('div');
     container.appendChild(newElement);
+    newElement.addEventListener('mouseover', function (e) {
+      newElement.classList.add('hover');
+    })
   }
 }
